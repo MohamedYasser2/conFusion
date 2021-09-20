0f79bbc32c5d560308da4d5cb19c8c56cadd22c6
Mohamed Yasser <mohamed.y.ghanem@gmail.com>
2021-09-20 17:13:09 +0200

    Template driven forms part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -46,6 +50,10 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatDialogModule,
     AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule 
   ],
   entryComponents: [
     LoginComponent
